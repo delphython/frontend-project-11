4d0abff33ddea2d51c2b3f795a15e2a600cfca79
Vitaly Klyukin <delphython@gmail.com>
2024-03-03 15:57:43 +0300

    add app

---
+++ src/app.js
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+import 'bootstrap';
+import * as yup from 'yup';
+import onChange from 'on-change';
+import css from './styles.css';
+import render from './render.js';
+
+const runApp = () => {
+  const state = {
+    addingFeedsForm: {
+      paths: [],
+      valid: '',
+      errors: [],
+    },
+  };
+  const watchedState = onChange(state, render);
+
+  const schema = yup
+    .string().required().url().notOneOf(state.addingFeedsForm.paths);
+
+  const formElement = document.querySelector('.rss-form');
+  formElement.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = formData.get('url');
+    schema.validate(data, { abortEarly: false })
+      .then((path) => {
+        watchedState.addingFeedsForm = {
+          paths: state.paths.push(path),
+          valid: 'valid',
+          errorMessage: null,
+        };
+        // здесь будет выполняться асинхронный запрос на сервер,
+      })
+      .catch(({ errors }) => {
+        watchedState.addingFeedsForm = {
+          valid: 'invalid',
+          errors,
+        };
+      });
+  });
+};
+
+runApp();
